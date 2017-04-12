890c7b87aeca07d86cbefbac9d5653398f85c42d
evilgod <egod1430@gmail.com>
2017-04-12 15:35:06 -0700

    twosum

---
+++ twoSum/main.py
@@ -0,0 +1,5 @@
+import twosum
+nums = [2, 7, 11, 15]
+target = 9
+result=twosum.Solution()
+print result.twoSum(nums, target)
---
+++ twoSum/twosum.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, num, target):
+        dict1={}
+        for i in range(len(num)):
+            if dict1.get(target-num[i], "No") == "No":
+            
+                dict1[num[i]]=i
+            else:
+                return (dict1[target-num[i]]+1, i+1)
+            
+if __name__== "__main__":
+
+    print Solution().twoSum([2, 7, 11, 15], 9)
